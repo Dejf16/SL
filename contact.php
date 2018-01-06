194ea4eb7580e9e68216f1fdbfa343609820f061
Dejf16 <donovaldavid@gmail.com>
2018-01-06 22:06:55 +0100

    dady

--- contact.php
+++ contact.php
@@ -13,28 +13,24 @@
     <div class="wrap">
         <div class="header">
             <div class="logo">
-                <a href="index.html"><img src="images/logo.png" alt="" title="logo"></a>
-            </div>
-            <div class="search">
-                <form>
-                    <input type="text" value="">
-                    <input type="submit" value="">
-                </form>
+                <a href="index.html"><img src="images/logo1.png" alt="" title="logo"></a>
             </div>
             <div class="drp-dwn">
                 <select onchange="window.location=this.options[this.selectedIndex].value">
-                    <option value="index.html">Home</option>
-                    <option value="about.html">About</option>
-                    <option value="service.html">Service</option>
-                    <option value="contact.html">Contact</option>
+                    <option value="index.html">Úvod</option>
+                    <option value="byty.html">Byty</option>
+                    <option value="about.html">O nas</option>
+                    <option value="service.html">STKcka</option>
+                    <option value="contact.php">Kontakt</option>
                 </select>
             </div>
             <div class="menu">
                 <ul class="nav">
-                    <li><a href="index.html">Home</a></li>
-                    <li><a href="about.html">About</a></li>
-                    <li><a href="service.html">Services</a></li>
-                    <li  class="active"><a href="contact.html">Contact</a></li>
+                    <li ><a href="index.html">Úvod</a></li>
+                    <li><a href="byty.html">Byty</a></li>
+                    <li><a href="about.html">O nás</a></li>
+                    <li><a href="//http:www.donivostk.sk">STKcka</a></li>
+                    <li class="active"><a href="contact.php">Kontakt</a></li>
                 </ul>
             </div>
             <div class="clear"></div>
@@ -47,7 +43,7 @@
             <div class="section group">
                 <div class="col span_1_of_3">
                     <div class="contact_info">
-                        <h3>Find Us Here:</h3>
+                        <h3>Nájdete nás:</h3>
                         <div class="map">
                             <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2632.6408924789757!2d19.1306194!3d48.7123444!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47153dda353456d3%3A0xca5e6b146496d369!2sSl%C3%A1dkovi%C4%8Dova+4370%2F27%2C+974+05+Bansk%C3%A1+Bystrica!5e0!3m2!1ssk!2ssk!4v1513957685681" width="450" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
                         </div>
@@ -66,7 +62,7 @@
                 </div>
                 <div class="col span_2_of_3">
                     <div class="contact-form">
-                        <h3>Contact Us</h3>
+                        <h3>Kontaktujte nás</h3>
                         <form action="">
                             <div>
                                 <span><label>NAME</label></span>
@@ -111,35 +107,23 @@ if(isset($_POST['submit'])){
         <div class="footer">
             <div class="section group">
                 <div class="col_1_of_4 span_1_of_4">
-                    <h3>Follow Us</h3>
+                    <h3>Sledujte nas</h3>
                     <ul class="nav1">
                         <li><a href="#"><img src="images/facebook.png" title="facebook" alt="" />Facebook</a></li>
-                        <li><a href="#"><img src="images/twitter.png" title="twitter" alt=""/>Twitter</a></li>
-                        <li><a href="#"><img src="images/gplus.png" title="googole+" alt=""/>Google+</a></li>
-                    </ul>
-                </div>
-                <div class="col_1_of_4 span_1_of_4">
-                    <h3>Information</h3>
-                    <ul class="nav1">
-                        <li><a href="#">Always free from repetition</a></li>
-                        <li><a href="#">Morbi blandit turpis ewuhre</a></li>
-                        <li><a href="#">Integer id ante nec elit mo</a></li>
-                        <li><a href="#">Maecenas accumsan lorem sed</a></li>
                     </ul>
                 </div>
 
                 <div class="col_1_of_4 span_1_of_4">
-                    <h3>Contact Us</h3>
+                    <h3>Kontakt</h3>
                     <ul class="nav2">
-                        <li>22-56-2-9 Sit Amet, Lorem,</li>
-                        <li>USA</li>
-                        <li>Phone:(00) 222 666 444 </li>
-                        <li>Email:<a href="mailto:info@gmail.com"><span> info@mycompany.com</span></a></li>
+                        <li>Banska Bystrica</li>
+                        <li>Slovensko</li>
+                        <li>Telefon:0901 111 111</li>
+                        <li>Email:<a href="mailto:stk@stk.sk"><span> stk@stk.sk</span></a></li>
                     </ul>
                 </div>
                 <div class="col_1_of_4 span_1_of_4">
-                    <a href="#"><img src="images/logo.png" alt="" /></a>
-                    <p class="nav1">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                    <a href="#"><img src="images/logo1.png" alt="" /></a>
                 </div>
             </div>
         </div>
@@ -147,10 +131,6 @@ if(isset($_POST['submit'])){
 </div>
 <div class="footer1-bg">
     <div class="wrap">
-        <div class="footer1">
-            <p></p>
-            <p class="w3-link">© 2013 land_agent. All rights reserved | Design by <a href="http://w3layouts.com/">W3layouts</a></p>
-        </div>
     </div>
 </div>
 </body>
